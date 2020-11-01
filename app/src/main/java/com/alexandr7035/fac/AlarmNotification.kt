e9f489e70613cdff230519d2c436589d93511a05
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-11-01 21:01:20 +0300

    Add playSound method

--- app/src/main/java/com/alexandr7035/fac/AlarmNotification.kt
+++ app/src/main/java/com/alexandr7035/fac/AlarmNotification.kt
@@ -1,9 +1,11 @@
 package com.alexandr7035.fac
 
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.alexandr7035.fac.db.AlarmEntity
+import com.alexandr7035.fac.services.SoundService
 
 class AlarmNotification(private var context: Context, alarm: AlarmEntity) {
 
@@ -13,10 +15,12 @@ class AlarmNotification(private var context: Context, alarm: AlarmEntity) {
     private var notificationId: Int = 0
 
     init {
+        // Set params
         builder.setContentTitle(alarm.name)
         builder.setContentText("Будильник - ${alarm.hours}:${alarm.minutes}")
         builder.setSmallIcon(R.drawable.ic_alarm_clock)
         builder.priority = NotificationCompat.PRIORITY_MAX
+
 
         notificationId = genId()
 
@@ -26,6 +30,8 @@ class AlarmNotification(private var context: Context, alarm: AlarmEntity) {
         with(NotificationManagerCompat.from(context)) {
             // notificationId is a unique int for each notification that you must define
             notify(notificationId, builder.build())
+
+            playSound()
         }
     }
 
@@ -34,6 +40,11 @@ class AlarmNotification(private var context: Context, alarm: AlarmEntity) {
     // Could be improved
     private fun genId(): Int {
         return  (0..32000).random()
+    }
+
+
+    private fun playSound() {
+        context.startService(Intent(context, SoundService::class.java))
     }
 
 }
